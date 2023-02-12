5e5574fef66620a0621bf82dda0a6fd26abf7d72
Barath J <barath2001.j@gmail.com>
2023-02-12 06:33:32 +0530

    routing setup, add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,39 @@
+import react from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import RootLayout from "./components/RootLayout";
+import HomePage from "./pages/HomePage";
+import BlogPage from "./pages/BlogPage";
+import AboutPage from "./pages/AboutPage";
+import AuthPage from "./pages/AuthPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/auth",
+        element: <AuthPage />,
+      },
+      {
+        path: "/blog/:id",
+        element: <BlogPage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
